160d5ba4157d4dec14317fbcf0b4d83956e0e3e3
Ethan Kim <fateindestiny.dev@gmail.com>
2024-01-08 13:39:14 +0900

    feat: Added page back function in router sample

--- src/ui/pages/router/RouterContainer.js
+++ src/ui/pages/router/RouterContainer.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
+import {RippleButton} from '../../components/Button';
+import {useRouter} from '../../../hooks';
 
 const RouterContainer = () => {
-  return <div>
-    <FormattedMessage id={'routerSample.subPageTitle'} />
-  </div>
-}
+  const {navigate} = useRouter();
+  return (
+    <div className="wrap">
+      <p className="title">
+        <FormattedMessage id={'routerSample.subPageTitle'} />
+      </p>
+      <div style={{marginTop: '10px'}}>
+        <RippleButton onClick={() => navigate(-1)}>Move back</RippleButton>
+      </div>
+    </div>
+  );
+};
 
-export default RouterContainer
+export default RouterContainer;
